b7db2deef3c53f245f57429fc1774aaef3a6fe3a
John Wetherill <jwetherill@saks.com>
2021-10-28 09:44:57 -0700

    set only single var

--- build/tag-s3-object.sh
+++ build/tag-s3-object.sh
@@ -23,7 +23,8 @@ case "${GO_PIPELINE_NAME}" in
     bw-jw-build)
       export BuildTimestamp=$(date "+%Y-%m-%d %H:%M:%S")
       export BuiltBy=${GO_TRIGGER_USER}
-      jq '.TagSet += [{"Key": "BuildTimeStamp", "Value": "'${BuildTimestamp}'"},{"Key": "BuiltBy", "Value": "'${BuiltBy}'"}]' RemoveVersion.json > AddedKey.json
+#      jq '.TagSet += [{"Key": "BuildTimeStamp", "Value": "'${BuildTimestamp}'"},{"Key": "BuiltBy", "Value": "'${BuiltBy}'"}]' RemoveVersion.json > AddedKey.json
+      jq '.TagSet += [{"Key": "BuildTimeStamp", "Value": "'${BuildTimestamp}'"}'"}]' RemoveVersion.json > AddedKey.json
       ;;
     bw-jw-dev-etl)
       ;;
