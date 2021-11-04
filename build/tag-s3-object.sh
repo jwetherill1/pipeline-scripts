887203e7f2cfe9a4a7ffe64f1a93c6d0930efdf0
John Wetherill <jwetherill@saks.com>
2021-11-04 09:50:05 -0400

    hard-code jar #12

--- build/tag-s3-object.sh
+++ build/tag-s3-object.sh
@@ -1,58 +1,55 @@
 #!/bin/bash
 # tag jar object in S3
+
+# @param $1 AWS tag key, $2 json, $3 AWS tag value
 replaceTag () {
-      json=$(jq 'del(.TagSet[] | select(.Key == "'$1'"))' <<< "$2")
-      json=$(jq '.TagSet += [{"Key": "'$1'", "Value": "'$3'"}]' <<< "$json")
+    echo $1 $2 $3
+    json=$(echo "$2" | jq 'del(.TagSet[] | select(.Key == "'$1'"))')
+    json=$(echo "$json" | jq --arg output "${3}" '.TagSet += [{"Key": "'$1'", "Value": "\($output)"}]')
+    echo "Replace Tag: "  $json
 }
 
-addFirstRunTag () {
-      if [ -z "$(jq '.TagSet[] | select(.Key=="'$1'").Value' <<< "$2")" ]
-      then
-        json=$(jq '.TagSet += [{"Key": "'$1'", "Value": "'$3'"}]' <<< "$json")
-      fi
+# @param $1 AWS tag key, $2 json, $3 AWS tag value
+addTag () {
+    if [ -z "$(echo "$2" |  jq '.TagSet[] | select(.Key=="'$1'").Value')" ]; then
+        json=$(echo "$json" | jq --arg output "${3}" '.TagSet += [{"Key": "'$1'", "Value": "\($output)"}]')
+    fi
 }
 
-newjar=Autocomplete_Index_Builder-assembly-8-fc5bdc6-1.0.jar
+#newjar=Autocomplete_Index_Builder-assembly-$GO_PIPELINE_LABEL-1.0.jar
+newjar=Autocomplete_Index_Builder-assembly-12-33adb64-1.0.jar
 aws_path=search/autocomplete/artifacts
-bucket='saks-ml'
 
-aws sts assume-role --role-arn arn:aws:iam::326027360148:role/EMR_EC2_DefaultRole --role-session-name gocd-run-emr-etl > response.json
-export AWS_SECRET_ACCESS_KEY=$( cat response.json | python3 -c "import sys, json; print(json.load(sys.stdin)['Credentials']['SecretAccessKey'])")
-export AWS_ACCESS_KEY_ID=$( cat response.json | python3 -c "import sys, json; print(json.load(sys.stdin)['Credentials']['AccessKeyId'])")
-export AWS_SESSION_TOKEN=$( cat response.json | python3 -c "import sys, json; print(json.load(sys.stdin)['Credentials']['SessionToken'])")
-rm response.json
+aws_creds=$( aws sts assume-role --role-arn arn:aws:iam::326027360148:role/EMR_EC2_DefaultRole --role-session-name gocd-run-emr-etl )
+export AWS_SECRET_ACCESS_KEY=$( echo ${aws_creds} | jq --raw-output .Credentials.SecretAccessKey)
+export AWS_ACCESS_KEY_ID=$( echo ${aws_creds} | jq --raw-output .Credentials.AccessKeyId)
+export AWS_SESSION_TOKEN=$( echo ${aws_creds} | jq --raw-output .Credentials.SessionToken)
 
-json=$(aws s3api get-object-tagging --bucket ${bucket} --key "search/autocomplete/artifacts/Autocomplete_Index_Builder-assembly-8-fc5bdc6-1.0.jar")
-json=$(jq 'del(.VersionId)' <<< "$json")
+json=$(aws s3api get-object-tagging --bucket $S3_BUCKET --key $aws_path/$newjar)
+json=$(echo "$json" | jq 'del(.VersionId)')
 
-export Timestamp=$(date "+%Y-%m-%d:%H:%M:%S")
+export timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+echo ${GO_PIPELINE_NAME}
 
-case "${GO_PIPELINE_NAME}" in
-    bw-jw-build)
-      replaceTag "Build" "${json}" "${Timestamp}"
-      ;;
-    bw-jw-dev-etl | bw-jw-dev-etl-cron)
-      replaceTag "DevLastRun" "${json}" "${Timestamp}"
-      addFirstRunTag "DevFirstRun" "${json}" "${Timestamp}"
-      ;;
-    bw-jw-stage-etl | bw-jw-stage-etl-cron)
-      replaceTag "StageLastRun" "${json}" "${Timestamp}"
-      addFirstRunTag "StageFirstRun" "${json}" "${Timestamp}"
-      ;;
-    bw-jw-prod-etl | bw-jw-prod-etl-cron)
-      replaceTag "ProdLastRun" "${json}" "${Timestamp}"
-      addFirstRunTag "ProdFirstRun" "${json}" "${Timestamp}"
-      addFirstRunTag "ProdRunCount" "${json}" "1"
 
-      if [ -n "$(jq '.TagSet[] | select(.Key=="ProdRunCount").Value' <<< "$json")" ]
-      then
-        tagCount=$(jq '.TagSet[] | select(.Key=="ProdRunCount").Value | tonumber | .+1 '  <<< "$json")
-        json=$(jq 'del(.TagSet[] | select(.Key == "ProdRunCount"))' <<< "$json")
-        json=$(jq '.TagSet += [{"Key": "ProdRunCount", "Value": "'${tagCount}'"}]' <<< "$json")
-      fi
-      ;;
-    *)
-      echo "GO_PIPELINE_NAME IS ${GO_PIPELINE_NAME}"
-  esac
-cat <<< "$json" > final.json
-aws s3api put-object-tagging  --bucket $bucket --key "search/autocomplete/artifacts/Autocomplete_Index_Builder-assembly-8-fc5bdc6-1.0.jar" --tagging file://final.json
+if [[ "${GO_PIPELINE_NAME}" == "autocomplete-build" ]]; then
+    echo "got here"
+    replaceTag "Build" "${json}" "${timestamp}"
+elif [[ "${GO_PIPELINE_NAME}" == *"dev"* ]]; then    
+    replaceTag "DevLastRun" "${json}" "${timestamp}"
+    addTag "DevFirstRun" "${json}" "${timestamp}" 
+elif [[ "${GO_PIPELINE_NAME}" == *"stage"* ]]; then
+    replaceTag "StageLastRun" "${json}" "${timestamp}"
+    addTag "StageFirstRun" "${json}" "${timestamp}"
+elif [[ "${GO_PIPELINE_NAME}" == *"prod"* ]]; then
+    replaceTag "ProdLastRun" "${json}" "${timestamp}"
+    addTag "ProdFirstRun" "${json}" "${timestamp}"
+    if [ -n "$(echo "$json" | jq '.TagSet[] | select(.Key=="ProdRunCount").Value')" ]; then
+        tagCount=$(echo "$json" | jq '.TagSet[] | select(.Key=="ProdRunCount").Value | tonumber | .+1 ')
+        replaceTag "ProdRunCount" "${json}" "${tagCount}"
+    else
+        addTag "ProdRunCount" "${json}" "1"
+    fi
+fi
+echo "GO_PIPELINE_NAME IS ${GO_PIPELINE_NAME}"
+aws s3api put-object-tagging  --bucket $S3_BUCKET --key $aws_path/$newjar --tagging "$json"
